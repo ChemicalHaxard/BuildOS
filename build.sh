b28a4269d63be9042d30127d1b613a77a01d28d4
ImSpiDy <SpiDy2713@gmail.com>
2023-08-10 11:41:33 +0530

    F | R
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 . build/envsetup.sh
 lunch derp_lavender-userdebug
 
-build_gapps=1
+#build_gapps=1
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
 #export BUILD_BROKEN_DUP_RULES=true
 export RELAX_USES_LIBRARY_CHECK=true
-export WITH_GMS=false
+#export WITH_GMS=false
 
 exp_gapps() {
 export USE_GAPPS=true
