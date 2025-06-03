a1e463e8b8ea6485683052efc84e5d24df3b0c89
wHo-EM-i <ehteshammalik18998@gmail.com>
2025-06-03 21:43:52 +0530

    ci: los a10 exp | R
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- build.sh
+++ build.sh
@@ -1,11 +1,20 @@
 # Normal build steps
 . build/envsetup.sh
+# RBE
+#. /tmp/ci/rbe
+#export NINJA_REMOTE_NUM_JOBS=150
+#export RBE_CXX_LINKS_EXEC_STRATEGY=local
+#export RBE_METALAVA_EXEC_STRATEGY=local
+#export RBE_LOG_LEVEL=debug
+#export USE_CCACHE=0
 lunch lineage_lavender-userdebug
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
 export RELAX_USES_LIBRARY_CHECK=true
+export TARGET_KERNEL_VERSION=4.4
+#export USE_CCACHE=0
 
 build_gapps=0
 export GAPPS=false
@@ -16,5 +25,5 @@ export GAPPS=true
 }
 
 compile_plox () {
-mka bacon -j$(nproc --all)
+make bacon -j16
 }
